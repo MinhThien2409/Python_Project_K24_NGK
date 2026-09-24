23480b6c955c24a96b47a2c342dba3dd5616365c
Thien <3124410337@sv.sgu.edu.vn>
2026-09-24 09:05:21 +0700

    .

---
+++ back_end/Model/DanhMuc.py
@@ -0,0 +1,5 @@
+class DanhMuc:
+    def __init__(self, CategoryId=None, CategoryName=None, PlatformFeePercent=0):
+        self.CategoryId          = CategoryId
+        self.CategoryName        = CategoryName
+        self.PlatformFeePercent  = PlatformFeePercent
